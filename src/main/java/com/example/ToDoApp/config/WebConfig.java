d909edd1290136bf52fab3ad0954a6fc3aa66f21
HO NGUYEN Nhat-Quan <hnnhat67@gmail.com>
2025-05-15 17:05:08 +0200

    ✨ Mise à jour de la configuration CORS pour autoriser l'origine https://todoappnhat.netlify.app en plus de http://localhost:4200.

--- src/main/java/com/example/ToDoApp/config/WebConfig.java
+++ src/main/java/com/example/ToDoApp/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://todoappnhat.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
